b8719e1464a5601c805147bceea6845d5a5ad447
sys <1052856576@qq.com>
2016-03-27 00:32:58 +0800

    ADD: 1. 增加UIColor+MS.h          2. 重写viewController(正在写中)

---
+++ MusicStory/Other/extension/UIColor+MS.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+MS.h
+//  MusicStory
+//
+//  Created by sys on 16/3/27.
+//  Copyright © 2016年 sys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (MS)
+
+@end
---
+++ MusicStory/Other/extension/UIColor+MS.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+MS.m
+//  MusicStory
+//
+//  Created by sys on 16/3/27.
+//  Copyright © 2016年 sys. All rights reserved.
+//
+
+#import "UIColor+MS.h"
+
+@implementation UIColor (MS)
+
+@end
